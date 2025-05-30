1f04a6d439fa13a20603ba0b1835f9bd9e9a566c
“Ali <tadayoni.ali.enayat@gmail.com>
2025-05-30 16:53:14 +0330

    create slugs

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const slugify = require("slugify");
+
 const replaceTemplate = require("./modules/replaceTemplate");
 
 // Top level code that only execute once we start the program
@@ -11,6 +13,8 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 
 //  Create the server
 const server = http.createServer((req, res) => {
